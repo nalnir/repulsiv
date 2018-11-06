afcd6d785ecddf3e66a67b48b060573a402eb141
Lukas Stateczny <statecznylukas@gmail.com>
2018-11-06 18:24:54 +0000

    aws deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         // First application
         {
             name: 'ye',
-            script: 'sudo node server/inde.js',
+            script: 'sudo node server/index.js',
             env: {
                 COMMON_VARIABLE:
                     'true'
