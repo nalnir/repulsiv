969334c120f7c7a1d76f45b7abee9aa6bdcef1b9
Lukas Stateczny <statecznylukas@gmail.com>
2018-11-08 02:57:05 +0000

    testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         // First application
         {
             name: 'ye',
-            script: 'sudo node ./server/index.js',
+            script: './server/index.js',
             env: {
                 COMMON_VARIABLE:
                     'true'
